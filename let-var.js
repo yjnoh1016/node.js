9e804ee0842eee778f2c63a680dc89384cff90b2
노연주 <yjnoh1016@naver.com>
2021-10-18 13:35:19 +0900

    211018

---
+++ let-var.js
@@ -0,0 +1,8 @@
+let fruit = 'apple';
+console.log('fruit:', fruit);
+fruit = 'grape';
+console.log('fruit:', fruit);
+let fruit = 'hello'; // 재선언 불가
+var variable1 = 'hello';
+var variable1 = 'bye'; // 재선언 가능
+console.log(variable1);
