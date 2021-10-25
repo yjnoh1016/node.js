4de8bdaf46e0cca8f5fe09086cc0d91ca23ba445
노연주 <yjnoh1016@naver.com>
2021-10-25 12:11:35 +0900

    211025

---
+++ functions.js
@@ -0,0 +1,20 @@
+// function 작성방법
+function add1(x, y) {
+  return x + y;
+}
+
+// function 대신 화살표 함수 사용
+const add2 = (x, y) => {
+  return x + y;
+};
+
+const add3 = (x, y) => x + y;
+
+const add4 = (x, y) => x + y;
+
+//매개변수가 1개인 경우 괄호 생략 가능함.
+function not1(x) {
+  return !x;
+}
+
+const not2 = (x) => !x;
